00c33c8712f6fcf24deb9510a1efe71eda64458b
Dastan Mukeev <55836145+thedastan@users.noreply.github.com>
2025-01-30 15:00:31 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['mych-bot.ch']
+		domains: ['kurak.online']
 
 	}
 		// domains: ['kurak.pythonanywhere.com']
