1a3a1e544fbcb6a98d5b8bf2ba292ef0402940d0
AsimMah <mahmudovasim799@gmail.com>
2025-08-19 15:49:14 +0600

    Update TrailerButton.tsx

--- src/components/ui/buttons/TrailerButton.tsx
+++ src/components/ui/buttons/TrailerButton.tsx
@@ -1,65 +1,37 @@
 'use client'
 
-import {
-	Button,
-	Modal,
-	ModalCloseButton,
-	ModalContent,
-	useDisclosure
-} from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import Link from 'next/link'
 import { VscTriangleRight } from 'react-icons/vsc'
 
 import { PropsWithTitle } from '@/types/props.types'
 
 const TrailerButton = ({ title }: PropsWithTitle) => {
-	const { isOpen, onClose, onOpen } = useDisclosure()
-	// const link =
-	// 	'https://drive.google.com/file/d/1f3hdrcsq3sA0sNyKEMdw-O7sFc466nwv/preview'
-
-	const link = 'https://www.youtube.com/embed/ojZXDzvEyH0?si=jlPrPbTdrV2E7Wue'
+	const link =
+		'https://drive.google.com/file/d/1pUEbvbMEpGeAJQWOjRXgUU541_8ukryN/view?usp=sharing'
 	return (
 		<>
-			<Button
-				onClick={onOpen}
-				variant='none'
-				minW='217px'
-				bg='#BD1A1A'
-				h='47.5px'
-				color='#FFFFFF'
-				fontSize='18px'
-				fontWeight='500'
-				px='40px'
-				lineHeight='25.2px'
-				gap='9px'
-				alignItems='center'
+			<Link
+				href={link}
+				target={'_blank'}
 			>
-				{title}
-				<VscTriangleRight color='#FFFFFF' />
-			</Button>
-
-			<Modal
-				isOpen={isOpen}
-				onClose={onClose}
-				size='full'
-			>
-				<ModalContent
-					bg='transparent'
-					h='100vh'
+				<Button
+					variant='none'
+					minW='217px'
+					bg='#BD1A1A'
+					h='47.5px'
+					color='#FFFFFF'
+					fontSize='18px'
+					fontWeight='500'
+					px='40px'
+					lineHeight='25.2px'
+					gap='9px'
+					alignItems='center'
 				>
-					<ModalCloseButton
-						left='20px'
-						top='20px'
-						color='#FFFFFF'
-						fontSize={{ md: '26px', base: '20px' }}
-					/>
-					<iframe
-						src={link}
-						width='100%'
-						height='100%'
-						allow='autoplay'
-					></iframe>
-				</ModalContent>
-			</Modal>
+					{title}
+					<VscTriangleRight color='#FFFFFF' />
+				</Button>
+			</Link>
 		</>
 	)
 }
