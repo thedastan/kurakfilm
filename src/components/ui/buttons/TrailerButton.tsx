bcb5002a91b50ef86303a927ed47c1499e424cae
AsimMah <mahmudovasim799@gmail.com>
2025-09-15 17:28:46 +0600

    youtube-video

--- src/components/ui/buttons/TrailerButton.tsx
+++ src/components/ui/buttons/TrailerButton.tsx
@@ -1,40 +1,66 @@
 'use client'
 
-import { Button } from '@chakra-ui/react'
-import Link from 'next/link'
+import {
+  Button,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  useDisclosure
+} from '@chakra-ui/react'
 import { VscTriangleRight } from 'react-icons/vsc'
 
 import { PropsWithTitle } from '@/types/props.types'
 
 const TrailerButton = ({ title }: PropsWithTitle) => {
-	const link =
-		'https://www.youtube.com/watch?v=Ji5qXSJMWo0'
-	return (
-		<>
-			<Link
-				href={link}
-				target={'_blank'}
-			>
-				<Button
-					variant='none'
-					minW='217px'
-					bg='#BD1A1A'
-					h='47.5px'
-					color='#FFFFFF'
-					fontSize='18px'
-					fontWeight='500'
-					px='40px'
-					lineHeight='25.2px'
-					gap='9px'
-					alignItems='center'
-				>
-					{title}
-					<VscTriangleRight color='#FFFFFF' />
-				</Button>
-			</Link>
-		</>
-	)
+  const { isOpen, onClose, onOpen } = useDisclosure()
+   
+
+  const link = 'https://www.youtube.com/embed/Ji5qXSJMWo0?si=eRBDg4No6ajKk4Nj'
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        variant='none'
+        minW='217px'
+        bg='#BD1A1A'
+        h='47.5px'
+        color='#FFFFFF'
+        fontSize='18px'
+        fontWeight='500'
+        px='40px'
+        lineHeight='25.2px'
+        gap='9px'
+        alignItems='center'
+      >
+        {title}
+        <VscTriangleRight color='#FFFFFF' />
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size='full'
+      >
+        <ModalContent
+          bg='transparent'
+          h='100vh'
+        >
+          <ModalCloseButton
+            left='20px'
+            top='20px'
+            color='#FFFFFF'
+            fontSize={{ md: '26px', base: '20px' }}
+          />
+          <iframe
+            src={link}
+            width='100%'
+            height='100%'
+            allow='autoplay'
+          ></iframe>
+        </ModalContent>
+      </Modal>
+    </>
+  )
 }
 
 export default TrailerButton
-
